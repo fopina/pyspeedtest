62092cb35ba758ddf85d885aedc49b664629e25c
Filipe Pina <fopina@skmobi.com>
2012-03-02 11:22:23 +0000

    ditched pycurl for builtin urllib (missing threaded downloads now)

--- pyspeedtest.py
+++ pyspeedtest.py
@@ -2,17 +2,18 @@
 
 '''
 TODO:
+- threaded downloading (like before with pycurl)
 - upload test
 - choose server based on latency (http://www.speedtest.net/speedtest-servers.php / http://SERVER/speedtest/latency.txt)
 '''
 
-import pycurl
+import urllib
 from time import time
 
 ###############
 
 HOST = 'http://speedtest.net.zon.pt'
-CONNECTIONS = 2
+RUNS = 2
 
 ###############
 
@@ -22,61 +23,20 @@ DOWNLOAD_FILES = [
 	('/speedtest/random1500x1500.jpg',4468241),
 ]
 
-m = None
-
-def ignore_data_cb(buf):
-	return len(buf)
-	
 def download():
 	total_start_time = time() * 1000
 	total_downloaded = 0
 	for (current_file, current_file_size) in DOWNLOAD_FILES:
-		num_processed = 0
-		connection = 1
-		total_downloaded += current_file_size * CONNECTIONS
-		for c in m.handles:
-			c.setopt(pycurl.URL, HOST + current_file + '?x=' + str(int(time()*1000)) + '&y=' + str(connection))
-			c.setopt(pycurl.WRITEFUNCTION, ignore_data_cb)
-			c.thread = connection
-			c.filename = current_file
-			m.add_handle(c)
-			connection += 1
-		while num_processed < CONNECTIONS:
-			while 1:
-			 	ret, num_handles = m.perform()
-				if ret != pycurl.E_CALL_MULTI_PERFORM:
-					break
-			while 1:
-				num_q, ok_list, err_list = m.info_read()
-				for c in ok_list:
-				 	m.remove_handle(c)
-					print 'Thread %d finished %s' % (c.thread, c.filename)
-				for c, errno, errmsg in err_list:
-				 	m.remove_handle(c)
-				 	print 'Fail WAT? final results might be unaccurate...'
-				num_processed = num_processed + len(ok_list) + len(err_list)
-				if num_q == 0:
-				 	break
-			m.select(1.0)
+		for run in range(RUNS):
+			total_downloaded += current_file_size
+			urllib.urlretrieve(HOST + current_file + '?x=' + str(int(time() * 1000)), '/dev/null')
+			print 'Run %d for %s finished' % (run, current_file)
 	total_ms = (time() * 1000) - total_start_time
 	print 'Took %d ms to download %d bytes' % (total_ms, total_downloaded)
 	print 'Download speed: ' + pretty_speed(total_downloaded * 8000 / total_ms)
 	
 def main():
 	download()
-	
-def init():
-	global m
-	m = pycurl.CurlMulti()
-	m.handles = []
-	for i in range(CONNECTIONS):
-	 	c = pycurl.Curl()
-	 	c.fp = None
-	 	c.setopt(pycurl.FOLLOWLOCATION, 1)
-	 	c.setopt(pycurl.MAXREDIRS, 5)
-	 	c.setopt(pycurl.CONNECTTIMEOUT, 30)
-	 	c.setopt(pycurl.TIMEOUT, 300)
-	 	m.handles.append(c)
 	
 def pretty_speed(speed):
 	units = [ 'bps', 'Kbps', 'Mbps', 'Gbps' ]
@@ -85,8 +45,6 @@ def pretty_speed(speed):
 		speed /= 1024
 		unit += 1
 	return str(speed) + ' ' + units[unit]
-
-init()
 
 if __name__ == '__main__':
 	main()
