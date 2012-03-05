61cb7c88d405c5abf20a9396f09a89789fe00552
Filipe Pina <fopina@skmobi.com>
2012-03-05 16:12:25 +0000

    upload() with httplib

--- pyspeedtest.py
+++ pyspeedtest.py
@@ -16,10 +16,10 @@ from threading import Thread, currentThread
 
 HOST = 'speedtest-po.vodafone.pt'
 RUNS = 2
+VERBOSE = 0
+HTTPDEBUG = 0
 
 ###############
-
-VERBOSE = 0
 
 DOWNLOAD_FILES = [
 	'/speedtest/random350x350.jpg',
@@ -36,9 +36,7 @@ def printv(msg):
 	if VERBOSE : print msg
 	
 def downloadthread(connection, url):
-	connection.putrequest('GET', url)
-	connection.putheader('Connection','Keep-Alive')
-	connection.endheaders()
+	connection.request('GET', url, None, { 'Connection': 'Keep-Alive'})
 	response = connection.getresponse()
 	self_thread = currentThread()
 	self_thread.downloaded = len(response.read())
@@ -48,6 +46,7 @@ def download():
 	connections = []
 	for run in range(RUNS):
 		connection = httplib.HTTPConnection(HOST)
+		connection.set_debuglevel(HTTPDEBUG)
 		connection.connect()
 		connections.append(connection)
 	total_start_time = time()
@@ -68,23 +67,30 @@ def download():
 	printv('Took %d ms to download %d bytes' % (total_ms, total_downloaded))
 	return (total_downloaded * 8000 / total_ms)
 
-def uploadthread(req):
-	response = urllib2.urlopen(req)
+def uploadthread(connection, req):
+	url = '/speedtest/upload.php?x=' + str(random())
+	connection.request('POST', url, req, {'Connection': 'Keep-Alive', 'Content-Type': 'application/x-www-form-urlencoded'})
+	response = connection.getresponse()
 	reply = response.read()
 	self_thread = currentThread()
 	self_thread.uploaded = int(reply.split('=')[1])
 	
 def upload():
-	url = 'http://' + HOST + '/speedtest/upload.php?x=' + str(random())
-	total_start_time = time()
+	connections = []
+	for run in range(RUNS):
+		connection = httplib.HTTPConnection(HOST)
+		connection.set_debuglevel(HTTPDEBUG)
+		connection.connect()
+		connections.append(connection)
 	total_uploaded = 0
+	
+	total_start_time = time()
 	for current_file_size in UPLOAD_FILES:
 		values = {'content0' : open('/dev/random').read(current_file_size) }
 		data = urllib.urlencode(values)
-		req = urllib2.Request(url, data)
 		threads = []
 		for run in range(RUNS):
-			thread = Thread(target = uploadthread, kwargs = { 'req': req })
+			thread = Thread(target = uploadthread, args = (connections[run], data))
 			thread.run_number = run
 			thread.start()
 			threads.append(thread)
@@ -93,12 +99,14 @@ def upload():
 			printv('Run %d for %d bytes finished' % (thread.run_number, thread.uploaded))
 			total_uploaded += thread.uploaded
 	total_ms = (time() - total_start_time) * 1000
+	for connection in connections:
+		connection.close()
 	printv('Took %d ms to upload %d bytes' % (total_ms, total_uploaded))
 	return (total_uploaded * 8000 / total_ms)
 
 def usage():
 	print '''
-usage: pyspeedtest.py [-h] [-v] [-r N] [-m N]
+usage: pyspeedtest.py [-h] [-v] [-r N] [-m M] [-d L]
 
 Test your bandwidth speed using Speedtest.net servers.
 
@@ -106,14 +114,15 @@ optional arguments:
  -h, --help         show this help message and exit
  -v                 enabled verbose mode
  -r N, --runs=N     use N runs (default is 2).
- -m N, --mode=N     test mode: 1 - download only, 2 - upload only, 3 - both (default)
+ -m M, --mode=M     test mode: 1 - download only, 2 - upload only, 3 - both (default).
+ -d L, --debug=L    set httpconnection debug level (default is 0).
 '''
 		
 def main():
-	global VERBOSE, RUNS
+	global VERBOSE, RUNS, HTTPDEBUG
 	mode = 3
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hr:vm:", ["help", "runs=","mode="])
+		opts, args = getopt.getopt(sys.argv[1:], "hr:vm:d:", ["help", "runs=","mode=","debug="])
 	except getopt.GetoptError, err:
 		print str(err)
 		usage()
@@ -136,6 +145,13 @@ def main():
 			except ValueError:
 				print 'Bad mode value'
 				sys.exit(2)
+		elif o in ("-d", "--debug"):
+			try:
+				HTTPDEBUG = int(a)
+			except ValueError:
+				print 'Bad debug value'
+				sys.exit(2)
+
 	if mode & 1 == 1:
 		print 'Download speed: ' + pretty_speed(download())
 	if mode & 2 == 2:
