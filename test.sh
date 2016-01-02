0f19be483d012e37b45324a64795be99ac2d893a
Six <brbsix@gmail.com>
2016-01-02 12:51:43 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -19,27 +19,48 @@ python setup.py install
 pyspeedtest
 
 # test simple command line arguments
-for opt in -h --help --version; do
-    pyspeedtest "$opt"
+for arg in -h --help --version; do
+    pyspeedtest "$arg"
 done
 
 # test http connection debug levels
 for level in 0 1 2 3 4 5; do
     pyspeedtest -d "$level" &>/dev/null
+    pyspeedtest --debug "$level" &>/dev/null
     pyspeedtest --debug="$level" &>/dev/null
 done
 
 # test various test modes
 for mode in 1 2 4 7; do
     pyspeedtest -m "$mode"
+    pyspeedtest --mode "$mode"
     pyspeedtest --mode="$mode"
 done
 
 # use numerous test runs
 for runs in 1 2 4 7; do
     pyspeedtest -r "$runs"
+    pyspeedtest --runs "$runs"
     pyspeedtest --runs="$runs"
 done
 
-# test multiple arguments at once
-[[ $(pyspeedtest) == $(pyspeedtest --debug=0 --mode=7 --runs=2) ]]
+# test multiple arguments at once and verify output
+pyspeedtest --debug=0 --mode=7 --runs=2 |& grep -qE $'^Using server: .*\nDownload speed: [0-9.]+ (bps|Kbps|Mbps|Gbps)\nUpload speed: [0-9.]+ (bps|Kbps|Mbps|Gbps)$'
+
+# test for bad arguments
+! pyspeedtest -x
+! pyspeedtest --xxx
+
+# test for bad modes
+for badmode in -1 0 8 9 10 11 12 13 14 15; do
+    for arg in -m --mode; do
+        ! output=$(pyspeedtest "${arg}" "${badmode}" 2>&1)
+        [[ $output = 'Bad mode value' ]]
+    done
+done
+
+# test for bad runs
+for arg in -r --runs; do
+    ! output=$(pyspeedtest "${arg}" xxx 2>&1)
+    [[ $output = 'Bad debug value' ]]
+done
