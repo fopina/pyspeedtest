cada5bd7fe0a3fbad998e2ea33f1e6f2023176f1
Six <brbsix@gmail.com>
2016-01-10 00:55:37 -0800

    Update test.sh with argparse error messages

--- test.sh
+++ test.sh
@@ -54,19 +54,22 @@ pyspeedtest --debug=0 --mode=7 --runs=2 |& grep -qE $'^Using server: .*\nDownloa
 # test for bad debug levels
 for arg in -d --debug; do
     ! output=$(pyspeedtest "${arg}" xxx 2>&1)
-    [[ $output = 'Bad debug level value' ]]
+    [[ $output = "usage: pyspeedtest [OPTION]...
+pyspeedtest.py: error: argument -d/--debug: invalid positive int value: 'xxx'" ]]
 done
 
 # test for bad modes
 for badmode in -1 0 8 9 10 11 12 13 14 15; do
     for arg in -m --mode; do
         ! output=$(pyspeedtest "${arg}" "${badmode}" 2>&1)
-        [[ $output = 'Bad mode value' ]]
+        [[ $output = "usage: pyspeedtest [OPTION]...
+pyspeedtest.py: error: argument -m/--mode: invalid choice: ${badmode} (choose from 1, 2, 3, 4, 5, 6, 7)" ]]
     done
 done
 
 # test for bad runs
 for arg in -r --runs; do
     ! output=$(pyspeedtest "${arg}" xxx 2>&1)
-    [[ $output = 'Bad runs value' ]]
+    [[ $output = "usage: pyspeedtest.py [OPTION]...
+pyspeedtest.py: error: argument -r/--runs: invalid positive int value: 'xxx'" ]]
 done
